347926039146f56db47637970a243f8ab2f495d8
Franklin Yin <franklinyin1@gmail.com>
2021-10-13 12:55:44 -0400

    styling: fridgeView and singleFridgeItemView

--- src/screens/Fridge/FridgeScreen.js
+++ src/screens/Fridge/FridgeScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import {
   View,
   Text,
@@ -6,10 +6,10 @@ import {
   ScrollView,
   Image,
   Button,
-} from 'react-native';
-import { useStorage } from '../../store/Context';
-import { fetchFridgeItems } from '../../store/reducers/fridgeReducer';
-import styles from './fridge-style';
+} from "react-native";
+import { useStorage } from "../../store/Context";
+import { fetchFridgeItems } from "../../store/reducers/fridgeReducer";
+import styles from "./fridge-style";
 
 function FridgeScreen({ navigation }) {
   const { fridgeState, dispatch, userState } = useStorage();
@@ -33,33 +33,45 @@ function FridgeScreen({ navigation }) {
                 <SafeAreaView style={styles.imageContainer}>
                   <Image style={styles.image} source={{ uri: item.imageUrl }} />
                 </SafeAreaView>
-                <Text>{item.name}</Text>
-                <Text>Servings: {item.servings}</Text>
-                <Text>
-                  Expiration Date:{' '}
-                  {new Date(
-                    item.expirationDate.seconds * 1000
-                  ).toLocaleDateString('en-US')}
-                </Text>
-                <Text>Allergens: {item.allergens}</Text>
-                <Text>Diet Flags: {item.dietFlags}</Text>
-                <Button
-                  title="Expand"
-                  onPress={() => {
-                    /* 1. Navigate to the Details route with params */
-                    navigation.navigate("Selected Item", {
-                      name: item.name,
-                      expirationDate: item.expirationDate,
-                      servings: item.servings,
-                      allergens: item.allergens,
-                      dietFlags: item.dietFlags,
-                      protein: item.protein,
-                      carbs: item.carbs,
-                      fat: item.fat,
-                      imageUrl: item.imageUrl,
-                    });
-                  }}
-                />
+                <SafeAreaView style={styles.otherData}>
+                  {/* capitalize first letter */}
+                  <Text style={styles.itemNameText}>
+                    {item.name.charAt(0).toUpperCase() + item.name.slice(1)}
+                  </Text>
+                  <Text> </Text>
+                  <Text>Servings: {item.servings}</Text>
+                  <Text>
+                    Expiration Date:{" "}
+                    {new Date(
+                      item.expirationDate.seconds * 1000
+                    ).toLocaleDateString("en-US")}
+                  </Text>
+                  <Text style={styles.baseText}>
+                    Allergens:{" "}
+                    {item.allergens.length ? item.allergens.join(", ") : "N/A"}
+                  </Text>
+                  <Text style={styles.baseText}>
+                    Diet Flags:{" "}
+                    {item.dietFlags.length ? item.dietFlags.join(", ") : "N/A"}
+                  </Text>
+                  <Button
+                    title="Expand"
+                    onPress={() => {
+                      /* 1. Navigate to the Details route with params */
+                      navigation.navigate("Selected Item", {
+                        name: item.name,
+                        expirationDate: item.expirationDate,
+                        servings: item.servings,
+                        allergens: item.allergens,
+                        dietFlags: item.dietFlags,
+                        protein: item.protein,
+                        carbs: item.carbs,
+                        fat: item.fat,
+                        imageUrl: item.imageUrl,
+                      });
+                    }}
+                  />
+                </SafeAreaView>
               </View>
             ))}
           </View>
