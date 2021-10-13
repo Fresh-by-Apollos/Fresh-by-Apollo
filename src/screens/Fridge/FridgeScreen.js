da93843a32e8e4e243be8ff7eead17fae62544c1
Franklin Yin <franklinyin1@gmail.com>
2021-10-13 16:16:35 -0400

    refactor: click route from fridgeView to singleItem placed on parent card

--- src/screens/Fridge/FridgeScreen.js
+++ src/screens/Fridge/FridgeScreen.js
@@ -6,6 +6,7 @@ import {
   ScrollView,
   Image,
   Button,
+  TouchableOpacity
 } from "react-native";
 import { useStorage } from "../../store/Context";
 import { fetchFridgeItems } from "../../store/reducers/fridgeReducer";
@@ -29,21 +30,33 @@ function FridgeScreen({ navigation }) {
         ) : (
           <View style={styles.notEmpty}>
             {fridgeState.map((item) => (
-              <View key={item.name} style={styles.fridgeItems} onPress={() => {
-                /* 1. Navigate to the Details route with params */
-                navigation.navigate("Selected Item", {
-                  name: item.name,
-                  expirationDate: item.expirationDate,
-                  servings: item.servings,
-                  allergens: item.allergens,
-                  dietFlags: item.dietFlags,
-                  protein: item.protein,
-                  carbs: item.carbs,
-                  fat: item.fat,
-                  imageUrl: item.imageUrl,
-                });
-              }}>
-                <SafeAreaView style={styles.imageContainer}>
+              //key is upcCode + expiration date
+              <TouchableOpacity
+                key={
+                  `${item.upcCode}` +
+                  new Date(
+                    item.expirationDate.seconds * 1000
+                  ).toLocaleDateString("en-US")
+                }
+                style={styles.fridgeItems}
+                onPress={() => {
+                  /* 1. Navigate to the Details route with params */
+                  navigation.navigate("Selected Item", {
+                    name: item.name,
+                    expirationDate: item.expirationDate,
+                    servings: item.servings,
+                    allergens: item.allergens,
+                    dietFlags: item.dietFlags,
+                    protein: item.protein,
+                    carbs: item.carbs,
+                    fat: item.fat,
+                    imageUrl: item.imageUrl,
+                  });
+                }}
+              >
+                <SafeAreaView
+                  style={styles.imageContainer}
+                >
                   <Image style={styles.image} source={{ uri: item.imageUrl }} />
                 </SafeAreaView>
                 <SafeAreaView style={styles.otherData}>
@@ -68,7 +81,7 @@ function FridgeScreen({ navigation }) {
                     {item.dietFlags.length ? item.dietFlags.join(", ") : "N/A"}
                   </Text>
                 </SafeAreaView>
-              </View>
+              </TouchableOpacity>
             ))}
           </View>
         )}
