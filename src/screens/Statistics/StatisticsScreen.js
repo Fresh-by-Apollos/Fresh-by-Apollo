22140a1b871768d1b6abb5998d1a968e975bb767
EricDaoNguyen <EricDaoNguyen@Gmail.com>
2021-10-15 17:22:30 -0400

    Fetched and displayed total macros.

--- src/screens/Statistics/StatisticsScreen.js
+++ src/screens/Statistics/StatisticsScreen.js
@@ -1,10 +1,52 @@
-import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Button, SafeAreaView, Text } from 'react-native'
+import { useStorage } from '../../store/Context'
 
-function StatisticsScreen({ route }) {
+function StatisticsScreen() {
+  const { fridgeState } = useStorage()
+  const [ totalMacros, setTotalMacros ] = useState({})
+
+  const fetchTotal = async(items) => {
+    let total = {
+      protein: 0,
+      carbs: 0,
+      fat: 0
+    }
+    items.forEach((item) => {
+      total.protein += item.protein
+      total.carbs += item.carbs
+      total.fat += item.fat
+    })
+    setTotalMacros(total)
+  }
+
+  useEffect(() => {
+    fetchTotal(fridgeState)
+  }, [])
+
+  // const fetchTotalProtein = async() => {
+  //   try {
+  //     const userId = firebase.auth().currentUser.uid;
+  //     const fridgeRef = firebase
+  //       .firestore()
+  //       .collection(`/users/${userId}/currentFridge`)
+  //     const proteinSnapshot = await fridgeRef.where('protein', '>', 0).get()
+  //     let total = 0
+  //     proteinSnapshot.forEach((doc) => {
+  //       total += doc.data().protein
+  //     })
+  //     return total
+  //   } catch (error) {
+  //     return `Error: ${error.message} || fetchTotalProtein`
+  //   }
+  // }
+
   return (
     <SafeAreaView>
-      <Text>Testing StatisticsScreen</Text>
+      <Text>Current Fridge Stats</Text>
+      <Text>Protein: {totalMacros.protein}</Text>
+      <Text>Carbs: {totalMacros.carbs}</Text>
+      <Text>Fat: {totalMacros.fat}</Text>
     </SafeAreaView>
   )
 }
