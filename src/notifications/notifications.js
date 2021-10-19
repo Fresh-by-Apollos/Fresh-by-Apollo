b97927c017a99fdb9307024cf806c1973396eabb
Franklin Yin <franklinyin1@gmail.com>
2021-10-19 11:46:08 -0400

    temporary commit

--- src/notifications/notifications.js
+++ src/notifications/notifications.js
@@ -6,6 +6,8 @@ import { Expo } from "expo-server-sdk";
 import * as admin from "firebase-admin";
 // import { initializeApp } from "firebase-admin/app";
 import { Storage } from "@google-cloud/storage";
+import firebase from "../firebase/firebase";
+import { differenceInCalendarDays } from "date-fns";
 
 // let expo = new Expo({ accessToken: process.env.EXPO_ACCESS_TOKEN });
 let expo = new Expo();
@@ -19,73 +21,126 @@ console.log("firestoreDb:", firestoreDb);
 
 firestoreDb.settings({ timestampsInSnapshots: true });
 
-const userId = "SifrqRnjtrMQQn3CWUXV9a4KMoS2";
+// const userId = "SifrqRnjtrMQQn3CWUXV9a4KMoS2";
 
-cron.schedule(
-  "*/1 * * * *",
-  async function loadPushNotifications() {
-    // Create the messages that you want to send to clients
+cron.schedule("*/1 * * * *", async function loadPushNotifications() {
+  // Create the messages that you want to send to clients
 
-    let messages = [];
+  let messages = [];
 
-    const userRef = firestoreDb.collection("users");
-    console.log("userRef:", userRef);
-    userRef.doc(userId).onSnapshot(async function (doc) {
-      console.log("doc.data():", doc.data());
-      let token = doc.data().expoPushToken;
-      console.log("token:", token);
-
-      let somePushTokens = [token];
-      for (let pushToken of somePushTokens) {
-        // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
-
-        // Check that all your push tokens appear to be valid Expo push tokens
-        if (!Expo.isExpoPushToken(pushToken)) {
-          console.error(
-            `Push token ${pushToken} is not a valid Expo push token`
-          );
-          continue;
+  const usersRef = firestoreDb.collection("users");
+  const usersSnapshot = await usersRef.get();
+  usersSnapshot.forEach(async (user) => {
+    let token = user.data().expoPushToken;
+    if (token) {
+      let userId = user.id;
+      let fridgeRef = firebase
+        .firestore()
+        .collection(`/users/${userId}/curentFridge`)
+        .orderBy("expirationDate", "asc");
+      const fridgeSnapshot = await fridgeRef.get();
+      let currDate = new Date();
+      for (let i = 0; i < fridgeSnapshot.length; i++) {
+        let fridgeItem = fridgeSnapshot[i];
+        let expirationDate = new Date(
+          fridgeItem.data().expirationDate.seconds * 1000
+        );
+        let timeDifference = differenceInCalendarDays(expirationDate, currDate);
+        if (timeDifference > 3) {
+          break;
+        } else if (timeDifference === 1 || timeDifference === 3) {
+          messages.push({
+            to: token,
+            sound: "default",
+            body: `${user.data().firstName} ${user.data().lastName}: Your item, ${
+              fridgeItem.data().name
+            }, is set to expire in ${timeDifference} days`,
+            data: { withSome: "data" },
+          });
         }
-        let date = new Date();
-        date = JSON.stringify(date);
-
-        // Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
-        messages.push({
-          to: pushToken,
-          sound: "default",
-          body: `The date is ${date}`,
-          data: { withSome: "data" },
-        });
       }
+    }
+  });
 
-      // The Expo push notification service accepts batches of notifications so
-      // that you don't need to send 1000 requests to send 1000 notifications. We
-      // recommend you batch your notifications to reduce the number of requests
-      // and to compress them (notifications with similar content will get
-      // compressed).
-      let chunks = expo.chunkPushNotifications(messages);
-      let tickets = [];
-      (async () => {
-        // Send the chunks to the Expo push notification service. There are
-        // different strategies you could use. A simple one is to send one chunk at a
-        // time, which nicely spreads the load out over time:
-        for (let chunk of chunks) {
-          try {
-            let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-            console.log(ticketChunk);
-            tickets.push(...ticketChunk);
-            // NOTE: If a ticket contains an error code in ticket.details.error, you
-            // must handle it appropriately. The error codes are listed in the Expo
-            // documentation:
-            // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
-          } catch (error) {
-            console.error(error);
-          }
-        }
-      })();
-    });
+  let chunks = expo.chunkPushNotifications(messages);
+  let tickets = [];
+  (async () => {
+    // Send the chunks to the Expo push notification service. There are
+    // different strategies you could use. A simple one is to send one chunk at a
+    // time, which nicely spreads the load out over time:
+    for (let chunk of chunks) {
+      try {
+        let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+        console.log(ticketChunk);
+        tickets.push(...ticketChunk);
+        // NOTE: If a ticket contains an error code in ticket.details.error, you
+        // must handle it appropriately. The error codes are listed in the Expo
+        // documentation:
+        // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  })();
+});
 
-    /*
+// console.log("userRef:", userRef);
+// userRef.doc(userId).onSnapshot(async function (doc) {
+//   console.log("doc.data():", doc.data());
+//   let token = doc.data().expoPushToken;
+//   console.log("token:", token);
+
+//   let somePushTokens = [token];
+//   for (let pushToken of somePushTokens) {
+// Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
+
+// Check that all your push tokens appear to be valid Expo push tokens
+// if (!Expo.isExpoPushToken(pushToken)) {
+//   console.error(
+//     `Push token ${pushToken} is not a valid Expo push token`
+//   );
+//   continue;
+// }
+// let date = new Date();
+// date = JSON.stringify(date);
+
+// Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
+// messages.push({
+//   to: pushToken,
+//   sound: "default",
+//   body: `The date is ${date}`,
+// data: { withSome: "data" },
+// });
+// }
+
+// The Expo push notification service accepts batches of notifications so
+// that you don't need to send 1000 requests to send 1000 notifications. We
+// recommend you batch your notifications to reduce the number of requests
+// and to compress them (notifications with similar content will get
+// compressed).
+// let chunks = expo.chunkPushNotifications(messages);
+// let tickets = [];
+// (async () => {
+// Send the chunks to the Expo push notification service. There are
+// different strategies you could use. A simple one is to send one chunk at a
+// time, which nicely spreads the load out over time:
+// for (let chunk of chunks) {
+//   try {
+//     let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+//     console.log(ticketChunk);
+//     tickets.push(...ticketChunk);
+// NOTE: If a ticket contains an error code in ticket.details.error, you
+// must handle it appropriately. The error codes are listed in the Expo
+// documentation:
+// https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     }
+//   })();
+// });
+
+/*
   // Instantiates a client. If you don't specify credentials when constructing
   // the client, the client library will look for credentials in the
   // environment.
@@ -107,7 +162,7 @@ cron.schedule(
   }
   listBuckets();
   */
-    /*
+/*
   // const userId = '1JFGaOjb95SefH2zlOJKz3ap17U2'
   // const userRef = firebase.firestore().collection('users')
   // let token
@@ -160,6 +215,5 @@ cron.schedule(
     }
   })();
 */
-  }
-  // loadPushNotifications();
-);
+
+// loadPushNotifications();
