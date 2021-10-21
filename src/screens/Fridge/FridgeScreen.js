e20dfe06244eba204e1b84bf96e6ad4810c49521
Francis Yu <francisjosephyu@gmail.com>
2021-10-21 15:31:04 -0400

    Fixed: merge conflict

--- src/screens/Fridge/FridgeScreen.js
+++ src/screens/Fridge/FridgeScreen.js
@@ -1,18 +1,15 @@
-import React, { useEffect, useState } from "react";
-import { FontAwesome } from "@expo/vector-icons";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import React, { useEffect, useState } from 'react';
+import { FontAwesome } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 
-import { Ionicons } from "@expo/vector-icons";
-
-import * as Notifications from "expo-notifications";
+import * as Notifications from 'expo-notifications';
 
 // This refers to the function defined earlier in this guide, in Push Notifications Set Up
 // import { registerForPushNotificationsAsync } from "../LoginScreen/SignUpScreen";
 
-import Constants from "expo-constants";
-import firebase from "../../firebase/firebase";
-
+import Constants from 'expo-constants';
+import firebase from '../../firebase/firebase';
 
 import {
   View,
@@ -47,45 +44,44 @@ function FridgeScreen({ navigation }) {
 
   //request permission to send Push Notifications
   const registerForPushNotificationsAsync = async () => {
-    console.log("entered registerForPushNotificationsAynsc");
+    console.log('entered registerForPushNotificationsAynsc');
     if (Constants.isDevice) {
       const { status: existingStatus } =
         await Notifications.getPermissionsAsync();
-      console.log("existingStatus:", existingStatus);
+      console.log('existingStatus:', existingStatus);
       let finalStatus = existingStatus;
-      if (existingStatus !== "granted") {
+      if (existingStatus !== 'granted') {
         const { status } = await Notifications.requestPermissionsAsync();
         finalStatus = status;
       }
-      if (finalStatus !== "granted") {
-        alert("Failed to get push token for push notification!");
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
         return;
       }
       const token = (await Notifications.getExpoPushTokenAsync()).data;
-      console.log("token:", token);
+      console.log('token:', token);
       console.log(
-        "firebase.auth().currentUser.uid",
+        'firebase.auth().currentUser.uid',
         firebase.auth().currentUser.uid
       );
       const userId = firebase.auth().currentUser.uid;
-      const userRef = firebase.firestore().collection("users");
-      const user = userRef.doc(userId)
-      await user.update({expoPushToken: token })
-        .catch((error) => {
-          alert(error);
-        });
-      const updatedUserRef = firebase.firestore().collection("users");
-      console.log("Set Expo Notification Token successfully");
+      const userRef = firebase.firestore().collection('users');
+      const user = userRef.doc(userId);
+      await user.update({ expoPushToken: token }).catch((error) => {
+        alert(error);
+      });
+      const updatedUserRef = firebase.firestore().collection('users');
+      console.log('Set Expo Notification Token successfully');
     } else {
-      alert("Must use physical device for Push Notifications");
+      alert('Must use physical device for Push Notifications');
     }
 
-    if (Platform.OS === "android") {
-      Notifications.setNotificationChannelAsync("default", {
-        name: "default",
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
         importance: Notifications.AndroidImportance.MAX,
         vibrationPattern: [0, 250, 250, 250],
-        lightColor: "#FF231F7C",
+        lightColor: '#FF231F7C',
       });
     }
   };
